b58caff632bbaddfea9747fe75bfbe957291136d
Joseph Innes <joseph.innes@swissinfo.ch>
2021-05-26 18:00:34 +0200

    Nav menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-// JS Goes here - ES6 supported
-
 import "./css/main.css";
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+const navLink = document.querySelectorAll("nav a");
+
+hamburger.addEventListener("click", toggleMobileMenu);
+navLink.forEach(link => link.addEventListener("click", closeMobileMenu));
+
+function toggleMobileMenu() {
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+}
+
+function closeMobileMenu() {
+    hamburger.classList.remove("active");
+    nav.classList.remove("active");
+}
