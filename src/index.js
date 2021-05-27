d1cb81dd9a3dbbdd7758c55ed1786ba8a343643d
Joseph Innes <joseph.innes@swissinfo.ch>
2021-05-27 15:11:54 +0200

    Update media grid & remove demo media

--- src/index.js
+++ src/index.js
@@ -16,3 +16,31 @@ function closeMobileMenu() {
     hamburger.classList.remove("active");
     nav.classList.remove("active");
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+    const lazyItems = [].slice.call(document.querySelectorAll(".lazy"));
+
+    const options = {
+        threshold: 0.5
+      }
+  
+    if (IntersectionObserver && lazyItems && lazyItems.length > 0) {
+        const lazyItemsObserver = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.style.backgroundImage = `url(${entry.target.dataset.srcset})`;
+                    entry.target.classList.remove('lazy');
+                    lazyItemsObserver.unobserve(entry.target);
+                }
+            });
+        }, options);
+  
+        lazyItems.forEach(lazyItem => {
+        lazyItemsObserver.observe(lazyItem);
+        });
+    } else {
+        lazyItems.forEach(lazyItem => {
+            lazyItem.style.backgroundImage = `url(${lazyItem.dataset.srcset})`;
+        });
+    }
+});
