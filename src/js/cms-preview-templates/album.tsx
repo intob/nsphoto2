83443270b9382fdfd687d373426456375c5d4a8a
Joseph Innes <joseph.innes@swissinfo.ch>
2021-06-02 15:40:04 +0200

    Replace babel & webpack with parcel

---
+++ src/js/cms-preview-templates/album.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import * as CMS from "netlify-cms-core";
+import * as format from "date-fns/format";
+
+export default class AlbumPreview extends React.Component<CMS.PreviewTemplateComponentProps> {
+  render() {
+    const {entry} = this.props;
+    let images = entry.getIn(["data", "images"]);
+    let videos = entry.getIn(["data", "videos"]);
+
+    const videosTemplate = videos.map((video: { getIn: (arg0: string[]) => any; }) => {
+      const file = video.getIn(["data", "video_file"]);
+      const poster = video.getIn(["data", "poster"]);
+      const videoUrl = `${window.parent.location.protocol}//${window.parent.location.host}/${file}`;
+      return <div className="media-grid-item">
+        <video src={videoUrl} poster={poster} controls playsInline /></div>;
+    });
+
+    const imagesTemplate = images.map((img: any) => {
+      const imageUrl = `${window.parent.location.protocol}//${window.parent.location.host}/${img}`;
+      return <div className="media-grid-item" style={{backgroundImage: `url(${imageUrl})`}}></div>;
+    });
+
+    return (
+      <>
+        <article>
+          <h1>{ entry.getIn(["data", "title"])}</h1>
+          <div className="metadata light">
+            <p>{ format(entry.getIn(["data", "date"]), "YYYY-MM-DD") }</p>
+          </div>
+          <p>{ entry.getIn(["data", "description"]) }</p>
+          <div className="media-grid">
+            { videosTemplate }
+            { imagesTemplate }
+          </div>
+        </article>
+      </>
+    );
+  }
+}
--- src/js/cms.tsx
+++ src/js/cms.tsx
@@ -1,15 +1,14 @@
-import React from "react";
 import CMS from "netlify-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
-import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
+import "../css/main.css";
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
 import AlbumPreview from "./cms-preview-templates/album";
 import ServicePreview from "./cms-preview-templates/service";
 
-CMS.registerPreviewStyle(styles, { raw: true });
+//CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("album", AlbumPreview);
