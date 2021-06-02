66158e9d8a43de06899ff815a5fe742f5f21834a
Joseph Innes <joseph.innes@swissinfo.ch>
2021-06-02 21:56:42 +0200

    Add live reload to dev server

--- dev-server.js
+++ dev-server.js
@@ -1,8 +1,19 @@
+const path = require("path");
 const express = require('express');
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
+
+const appPort = 3000;
+
 const app = express();
 
+app.use(connectLivereload());
 app.use(express.static('dist'));
 
-app.listen(3000, () => {
-    console.log('Listening on localhost:3000');
+app.listen(appPort, () => {
+    console.log(`DevServer: listening on localhost:${appPort}`);
 });
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'dist'));
+
