f72a8fdca5ec5722ffc8afca121bdd138217ad53
Joseph Innes <joseph.innes@swissinfo.ch>
2021-06-04 16:09:11 +0200

    Fix lazy-loading

--- src/js/lazy-load.js
+++ src/js/lazy-load.js
@@ -1,28 +1,56 @@
 export default function initLazyLoading() {
-  const lazyItems = [...document.querySelectorAll('[data-lazy]')];
+  const lazyImages = [...document.querySelectorAll('picture[data-lazy]')];
+  const lazyIframes = [...document.querySelectorAll('iframe[data-lazy]')];
 
   const observerOptions = {
     threshold: 0.3
   };
 
-  const lazyItemsObserver = new IntersectionObserver(entries => {
+  const lazyImageObserver = getLazyImageObserver(observerOptions);
+  lazyImages.forEach(lazyImage => {
+    lazyImageObserver.observe(lazyImage.parentElement);
+  });
+
+  const lazyIframeObserver = getLazyIframeObserver(observerOptions);
+  lazyIframes.forEach(lazyIframe => {
+    lazyIframeObserver.observe(lazyIframe);
+  });
+
+}
+
+function getLazyImageObserver(options) {
+  const intersectionObserver = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        entry.target.removeAttribute('data-lazy');
-        [...entry.target.querySelectorAll('[data-srcset]')].forEach(source => {
+        const lazyImage = entry.target.querySelector('[data-lazy]');
+        [...lazyImage.querySelectorAll('[data-srcset]')].forEach(source => {
           source.srcset = source.dataset.srcset;
-          source.removeAttribute('data-srcset');
+          if (source.tagName === 'IMG') {
+            source.addEventListener('load', () => {
+              lazyImage.removeAttribute('data-lazy');
+            });
+          }
         });
-        [...entry.target.querySelectorAll('[data-srcset-iframe]')].forEach(source => {
-          source.src = source.dataset.srcsetIframe;
-          source.removeAttribute('data-srcset-iframe');
-        });
-        lazyItemsObserver.unobserve(entry.target);
+        intersectionObserver.unobserve(entry.target);
       }
     });
-  }, observerOptions);
+  }, options);
+  return intersectionObserver;
+}
 
-  lazyItems.forEach(lazyItem => {
-    lazyItemsObserver.observe(lazyItem);
-  });
+function getLazyIframeObserver(options) {
+  const intersectionObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.src = entry.target.dataset.srcsetIframe;
+        console.log('iframe!');
+        entry.target.addEventListener('load', () => {
+          console.log('loaded');
+          entry.target.removeAttribute('data-lazy');
+        });
+        intersectionObserver.unobserve(entry.target);
+      }
+    });
+  }, options);
+  return intersectionObserver;
 }
